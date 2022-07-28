6aaf92451d54b80c0b3bee2b93983d9f685ed588
Andres Vasquez <you@example.com>
2022-07-27 22:21:51 -0400

    clase 4

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const app = express()
 
 const PORT = process.env.PORT || 3000
 
+app.use(express.json())
+
 app.use('/', index)
 app.use('/products', products)
 
--- routes/products.js
+++ routes/products.js
@@ -2,27 +2,71 @@ const express = require('express')
 
 const route = express.Router()
 
+var contId = 3
+var data = [
+    {
+        "id": 1,
+        "name": "libros",
+        "price": 10,
+        "stock": 100
+    },
+    {
+        "id": 2,
+        "name": "cuadernos",
+        "price": 11,
+        "stock": 200
+    }
+]
+
+// retorna todos los productos
 route.get('/', (req, res) => {
-    res.send('proxiammente: retornara todos los productos!')
+    res.json(data)
 })
 
+// retorna un producto
 route.get('/:id', (req, res) => {
     var { id } = req.params
-    res.send('proxiammente: retornara un producto con id: ' + id + ' !')
+    var result = data.find((item) => item.id == id)
+    res.json(result)
 })
 
+// guardar nuevo producto
 route.post('/', (req, res) => {
-    res.send('proxiammente: guardara un producto!')
+    var item = req.body
+    item.id = contId
+    data.push(item)
+    contId++
+    res.json({ 'state': 'ok' })
 })
 
+// editar producto
 route.put('/:id', (req, res) => {
     var { id } = req.params
-    res.send('proxiammente: editara un producto con id:' + id)
+    var newItem = req.body
+    var oldItem = data.find((item) => item.id == id)
+
+    if (newItem.name) {
+        oldItem.name = newItem.name
+    }
+    if (newItem.price) {
+        oldItem.price = newItem.price
+    }
+    if (newItem.stock) {
+        oldItem.stock = newItem.stock
+    }
+
+    data = data.map((item) => (item.id == oldItem.id) ? oldItem : item)
+
+    res.json({ 'state': 'ok' })
 })
 
+// eliminar producto
 route.delete('/:id', (req, res) => {
     var { id } = req.params
-    res.send('proxiammente: eliminara un producto con id:' + id)
+    var item = data.find((item) => item.id == id)
+    var index = data.indexOf(item)
+    data.splice(index, 1)
+    res.json({ 'state': 'ok' })
 })
 
 module.exports = route
